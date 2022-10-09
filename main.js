bd887196d981a282606c66b9d847fc1867f49be3
Sumbatyan <sumbatyanarmen@gmail.com>
2022-10-09 15:45:06 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-    jQuery(document).ready(function(){
-        console.log('ready');
-    })
+jQuery(document).ready(function(){
+    console.log('ready');
+})
 
-    jQuery('.select-area-wrapper .select').click(function(){
-        console.log(1);
-        jQuery('.select-popup').addClass('open')
-    })
+jQuery('.select-area-wrapper .select').click(function(){
+    console.log(1);
+    jQuery('.select-popup').addClass('open')
+})
