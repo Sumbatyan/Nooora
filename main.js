833cf2f615ccc72976a84715a6c190ad09f3a58d
Sumbatyan <sumbatyanarmen@gmail.com>
2022-10-10 01:44:30 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-jQuery(document).ready(function(){
-    console.log('ready');
+$('.select-area-wrapper .select').click(function(){
+    $('.select-popup').addClass('open')
 })
 
-jQuery('.select-area-wrapper .select').click(function(){
-    console.log(1);
-    jQuery('.select-popup').addClass('open')
+$('.select-popup .close, .select-popup .check-option').click(function(){
+    $('.select-popup').removeClass('open')
+})
+
+$('.select-area-wrapper .go').click(function(){
+    $('.second').addClass('active')
+})
+
+$('.back').click(function(){
+    $(this).closest('.container').parent().removeClass('active');
+    $('.swipe').removeClass('active')
+})
+
+$('.swipe').click(function(){
+    $(this).addClass('active')
+    setTimeout(function(){
+        $('.third').addClass('active')
+    }, 300)
+})
+
+$('.submit').click(function(){
+    $('.fourth').addClass('active')
+})
+
+$('.proceed').click(function(){
+    $('.fifth').addClass('active')
 })
